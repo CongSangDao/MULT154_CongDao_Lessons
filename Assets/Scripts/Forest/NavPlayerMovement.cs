fac3deb64ebd970fad26f43f64ec6b34466aec63
CongSangDao <sangdaovnb2002@gmail.com>
2024-03-22 22:48:46 -0700

    Unity AI & Navigation Lesson

--- Assets/Scripts/Forest/NavPlayerMovement.cs
+++ Assets/Scripts/Forest/NavPlayerMovement.cs
@@ -6,8 +6,24 @@ public class NavPlayerMovement : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
+    Rigidbody rbBody = null;
+    float trans = 0;
+    float rotate = 0;
+
+    public delegate void DropHive(Vector3 pos);
+    public static event DropHive DroppedHive;
+    
+    private void Start()
+    {
+        rbBody = GetComponent<Rigidbody>();
+    }
     void Update()
     {
+
+        if( Input.GetKeyDown(KeyCode.Space))
+        {
+            DroppedHive?.Invoke(transform.position + (transform.forward * 10));
+        }
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
